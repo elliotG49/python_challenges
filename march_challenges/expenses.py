a01d819251f3337fa3f8106d8fb0c54679a3cd79
elliotg <elliot@elliotg.co.uk>
2025-03-20 11:50:14 +0000

    changes to expenses

--- march_challenges/expenses.py
+++ march_challenges/expenses.py
@@ -1,5 +1,10 @@
 import pandas as pd
 
+def get_today_date():
+    from datetime import date
+    today = date.today()
+    return today
+
 def open_csv(path):
     expense_file = pd.read_csv(path)
     return expense_file
@@ -13,8 +18,19 @@ def get_total_expense(expense_file):
     print(total)
     return total
 
+def add_new_expense(expense_file):
+    data = []
+    data_types = {"date": datetime ,"account": ["Personal", "Business"], "catagory": str ,"note": str,"amount": float, "currency": float}
+    while input_val != limit:
+        for type in data_types:
+            user_input = input(f"Enter {type}: ")
+            if user_input == data_types[type].value():
+                data.append(input)
+                input_val += 1
+    expense_file.loc(data)
+            
 def main():
-    path = '/home/ellio/Desktop/python_challenges/march_challenges/files/expenses.csv'
+    path = '/home/elliotg/Desktop/python_challenges/march_challenges/files/expenses.csv'
     file = open_csv(path)
     display_columns(file)
     get_total_expense(file)
